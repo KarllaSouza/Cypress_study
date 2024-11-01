0ab69c75c5afc7c74807b89243bbc5725810b063
KarllaSouza <karlladanielly94@hotmail.com>
2024-11-01 01:17:40 +0000

    cypress - my tests - API

---
+++ cypress/e2e/api_testing/apiServerest.cy.js
@@ -0,0 +1,308 @@
+/// <reference types="cypress" />
+import ('../../support/commands.js')
+
+describe('Testes API - Usuário', () => {
+    //   const urlApi = 'https://serverest.dev/usuarios/';
+
+    context('Cenário 1 (Sucesso): fazer CRUD da API de usuário (Requests em testes separados)', () => {
+        it('Cadastrar usuário', () => {
+            cy.request({
+                url: 'https://serverest.dev/usuarios/', //'${urlApi}',
+                method: 'POST',
+                body: {
+                    "nome": "QA Teste",
+                    "email": "teste_1@qa.com",
+                    "password": "123",
+                    "administrador": "true"
+                }
+            }).then(response => {
+                expect(response.status).to.eq(201);
+                const userId = response.body._id; // Armazena o ID do usuário criado, que é retornado no corpo da resposta da API.
+                console.log('user ID: ' + response.body._id) // Exibe o ID do usuário criado no console, o que pode ser útil para depuração.
+                Cypress.env('userId', response.body._id) //Armazena o ID do usuário em uma variável de ambiente do Cypress (userId). Isso permite que outras partes do teste acessem este valor.
+                // '${Cypress.env(userId)}',
+                //    expect(response.body.usuarios[0].nome).to.equal('QA Teste')
+                expect(response.body.message).to.eq('Cadastro realizado com sucesso')
+            })
+        });
+
+        it('Consultar usuário - após cadastro', () => {
+            cy.request({
+                url: `https://serverest.dev/usuarios/${Cypress.env('userId')}`,
+                method: 'GET',
+            }).then(response => {
+                expect(response.status).to.eq(200);
+                console.log(response);
+
+                expect(response.body.nome).to.not.eq(undefined);
+                expect(response.body.email).to.not.eq(undefined);
+                expect(response.body.password).to.not.eq(undefined);
+                expect(response.body._id).to.not.eq(undefined);
+
+                expect(response.body.nome).to.eq('QA Teste');
+                expect(response.body.email).to.eq('teste_1@qa.com');
+                expect(response.body.password).to.eq('123');
+                expect(response.body.administrador).to.eq('true');
+
+                const userEmail = response.body.email;
+                const userPassword = response.body.password;
+
+                Cypress.env('userEmail', response.body.email);
+                Cypress.env('userPassword', response.body.password);
+            })
+        });
+
+        it('Editar usuário', () => {
+            cy.request({
+                url: `https://serverest.dev/usuarios/${Cypress.env('userId')}`,
+                method: 'PUT',
+                body: {
+                    "nome": "QA Teste_editado",
+                    "email": "teste_1_editado@qa.com",
+                    "password": "123_edicao",
+                    "administrador": "false"
+                }
+            }).then(response => {
+                expect(response.status).to.eq(200);
+                expect(response.body.message).to.eq('Registro alterado com sucesso')
+            })
+        });
+
+        it('Consultar usuário - após edição', () => {
+            cy.request({
+                url: `https://serverest.dev/usuarios/${Cypress.env('userId')}`,
+                method: 'GET',
+            }).then(response => {
+                expect(response.status).to.eq(200);
+                console.log(response);
+
+                expect(response.body.nome).to.eq('QA Teste_editado');
+                expect(response.body.email).to.eq('teste_1_editado@qa.com');
+                expect(response.body.password).to.eq('123_edicao');
+                expect(response.body.administrador).to.eq('false');
+
+                const userNameUpdated = response.body.nome;
+                const userEmailUpdated = response.body.email;
+                const userPasswordUpdated = response.body.password;
+                const userAdmUpdated = response.body.administrador;
+
+                Cypress.env('userNameUpdated', response.body.nome);
+                Cypress.env('userEmailUpdated', response.body.email);
+                Cypress.env('userPasswordUpdated', response.body.password);
+                Cypress.env('userAdmUpdated', response.body.administrador);
+            })
+        });
+
+        it('Deletar usuário', () => {
+            cy.request({
+                url: `https://serverest.dev/usuarios/${Cypress.env('userId')}`,
+                method: 'DELETE',
+                body: {}
+            }).then(response => {
+                expect(response.status).to.eq(200);
+                console.log(response);
+                expect(response.body.message).to.eq('Registro excluído com sucesso');
+            })
+        });
+
+        it('Consultar usuário - após exclusão', () => {
+            cy.request({
+                url: `https://serverest.dev/usuarios/${Cypress.env('userId')}`,
+                method: 'GET',
+                failOnStatusCode: false  // Evita que o Cypress falhe automaticamente em caso de status de erro
+            }).then(response => {
+                expect(response.status).to.eq(400);
+                console.log(response);
+                expect(response.body.message).to.eq('Usuário não encontrado');
+
+                expect(response.body.nome).to.eq(undefined);
+                expect(response.body.email).to.eq(undefined);
+                expect(response.body.password).to.eq(undefined);
+                expect(response.body._id).to.eq(undefined);
+            })
+        });
+    });
+
+    context('Cenário 2 (sucesso): Cadastro através do PUT', () => {
+        it('Editar usuário', () => {
+            cy.request({
+                url: `https://serverest.dev/usuarios/123123123`,
+                method: 'PUT',
+                body: {
+                    "nome": "QA Teste - criado no put",
+                    "email": "teste_1_editado-no-put@qa.com",
+                    "password": "teste123-put",
+                    "administrador": "true"
+                }
+            }).then(response => {
+                expect(response.status).to.eq(201);
+                expect(response.body.message).to.eq('Cadastro realizado com sucesso')
+                expect(response.body._id).to.not.equal(undefined)
+
+                const userId = response.body._id; // Armazena o ID do usuário criado
+                console.log('user ID: ' + response.body._id) // Exibe o ID do usuário criado no console
+                Cypress.env('userId', response.body._id) //Armazena o ID do usuário em uma variável de ambiente (userId).
+            })
+        });
+
+        it('Consultar usuário - após cadastro no put', () => {
+            cy.request({
+                url: `https://serverest.dev/usuarios/${Cypress.env('userId')}`,
+                method: 'GET',
+            }).then(response => {
+                expect(response.status).to.eq(200);
+                console.log(response);
+
+                expect(response.body.nome).to.eq('QA Teste - criado no put');
+                expect(response.body.email).to.eq('teste_1_editado-no-put@qa.com');
+                expect(response.body.password).to.eq('teste123-put');
+                expect(response.body.administrador).to.eq('true');
+            })
+        });
+
+        it('Deletar usuário', () => {
+            cy.request({
+                url: `https://serverest.dev/usuarios/${Cypress.env('userId')}`,
+                method: 'DELETE',
+                body: {}
+            }).then(response => {
+                expect(response.status).to.eq(200);
+                console.log(response);
+                expect(response.body.message).to.eq('Registro excluído com sucesso');
+            })
+        });
+
+        it('Consultar usuário - após exclusão', () => {
+            cy.request({
+                url: `https://serverest.dev/usuarios/${Cypress.env('userId')}`,
+                method: 'GET',
+                failOnStatusCode: false  // Evita que o Cypress falhe automaticamente em caso de status de erro
+            }).then(response => {
+                expect(response.status).to.eq(400);
+                console.log(response);
+                expect(response.body.message).to.eq('Usuário não encontrado');
+
+                expect(response.body.nome).to.eq(undefined);
+                expect(response.body.email).to.eq(undefined);
+                expect(response.body.password).to.eq(undefined);
+                expect(response.body._id).to.eq(undefined);
+            })
+        });
+    })
+
+    context.only('Cenário 3 (falha): cadastro de usuário com e-mail já utilizado', () => {
+        console.log('teste - cenário 3');
+        beforeEach(() => {
+            cy.setupUser();
+        });
+
+        it('cadastro com falha - e-mail já cadastrado', () => {
+            console.log('teste com e-mail duplicado');
+
+            cy.get('@user').then((user) => {
+
+                cy.request({
+                    url: 'https://serverest.dev/usuarios/',
+                    method: 'POST',
+                    body: {
+                        "nome": "QA Teste 2",
+                        "email": "teste_1@qa.com",
+                        "password": "1234",
+                        "administrador": "true"
+                    },
+                    failOnStatusCode: false
+                }).then(response => {
+                    expect(response.status).to.eq(400);
+                    expect(response.body.message).to.eq('Este email já está sendo usado');
+                });
+
+
+            });
+
+        });
+
+        afterEach(() => {
+            console.log('after - teardown !!!!!!!!!!!');
+            cy.teardownUser();
+        });
+        //Post cadastro - sucesso
+
+        //Post cadastro com mesmo e-mail
+        // it('Cadastrar usuário', () => {
+        //     cy.request({
+        //         url: 'https://serverest.dev/usuarios/',
+        //         method: 'POST',
+        //         body: {
+        //             "nome": "QA Teste",
+        //             "email": "teste_1@qa.com",
+        //             "password": "123",
+        //             "administrador": "true"
+        //         }
+        //     }).then(response => {
+        //         expect(response.status).to.eq(400);
+        //         expect(response.body.message).to.eq('Este email já está sendo usado')
+        //     })
+        // });
+
+        //Del deletar usuário com sucesso + get Consultar usuário deletado
+
+    });
+
+    context('Cenário 4 (falha): cadastro de usuário sem dados', () => {
+        //Post cadastro sem dados
+    })
+});
+
+
+describe('Teste API - Login', () => {
+    context('Cenário 3: fazer Login)', () => {
+        console.log('context');
+        beforeEach(() => {
+            console.log('before');
+            cy.setupUser();
+        })
+
+        it('Login com sucesso', () => {
+            cy.request({
+                url: 'https://serverest.dev/login/',
+                method: 'POST',
+                body: {
+                    "email": `${Cypress.env('userEmail')}`,
+                    "password": `${Cypress.env('userPassword')}`
+                },
+                // headers: `${Cypress.env('tokenAuthorization')}`
+            }).then(response => {
+                expect(response.status).to.eq(200);
+                console.log(response);
+                expect(response.body.message).to.eq('Login realizado com sucesso');
+                expect(response.body.authorization).to.not.eq(undefined);
+                const tokenAuthorization = response.body.authorization;
+                Cypress.env('tokenAuthorization', tokenAuthorization);
+                //    const authorization = `Bearer ${Cypress.env('ACCESS_TOKEN')}`
+            });
+        });
+
+        afterEach(() => {
+            console.log('tear down');
+            cy.teardownUser();
+        });
+    });
+
+})
+
+
+//       it('', () =>{
+//           cy.request({
+//               url: '',
+//               method: '',
+//               body: {}
+//           }).then(response => {
+//    expect(response.status).to.eq()
+//    console.log('user ID: '+response.body._id)
+//    Cypress.env('user_id', response._id)
+//           })
+//       });
+
+
+
+
---
+++ cypress/support/commands.js
@@ -0,0 +1,119 @@
+Cypress.Commands.add('setupUser', () => {
+    // it('Cadastrar usuário', () => {
+    console.log('setup');
+    cy.request({
+        url: 'https://serverest.dev/usuarios/', //'${urlApi}',
+        method: 'POST',
+        body: {
+            "nome": "QA Teste",
+            "email": "teste_1@qa.com",
+            "password": "123",
+            "administrador": "true"
+        }
+    }).then(response => {
+        expect(response.status).to.eq(201);
+        const userId = response.body._id; // Armazena o ID do usuário criado, que é retornado no corpo da resposta da API.
+        console.log(`user ID: ${userId}`) // Exibe o ID do usuário criado no console, o que pode ser útil para depuração.
+        Cypress.env('userId', userId) //Armazena o ID do usuário em uma variável de ambiente do Cypress (userId). Isso permite que outras partes do teste acessem este valor.
+        expect(Cypress.env('userId'), userId);
+
+        cy.request({
+            url: `https://serverest.dev/usuarios/${Cypress.env('userId')}`,
+            method: 'GET',
+        });
+
+    }).then(response => {
+        expect(response.status).to.eq(200);
+        console.log(response.body);
+        const userEmail = response.body.email;
+        const userPassword = response.body.password;
+        const userId = response.body._id;
+
+        expect(response.body.nome).to.not.eq(undefined);
+        expect(userEmail).to.not.eq(undefined);
+        expect(userPassword).to.not.eq(undefined);
+        expect(userId).to.not.eq(undefined);
+
+        expect(response.body.nome).to.eq('QA Teste');
+        expect(userEmail).to.eq('teste_1@qa.com');
+        expect(userPassword).to.eq('123');
+        expect(response.body.administrador).to.eq('true');
+
+        Cypress.env('userEmail', userEmail);
+        Cypress.env('userPassword', userPassword);
+        Cypress.env('userId', userId);
+
+        expect(userEmail).to.eq(Cypress.env('userEmail'));
+        expect(userPassword).to.eq(Cypress.env('userPassword'));
+        expect(userId).to.eq(Cypress.env('userId'));
+
+        cy.wrap({
+                id: userId,
+                email: userEmail,
+                password: userPassword
+            }
+        ).as('user');
+    });
+
+    // it('Consultar usuário - após cadastro', () => {
+    // cy.request({
+    //     url: `https://serverest.dev/usuarios/${Cypress.env('userId')}`,
+    //     method: 'GET',
+    // }).then(response => {
+    //     expect(response.status).to.eq(200);
+    //     console.log(response);
+    //     const userEmail = response.body.email;
+    //     const userPassword = response.body.password;
+    //
+    //     expect(response.body.nome).to.not.eq(undefined);
+    //     expect(userEmail).to.not.eq(undefined);
+    //     expect(userPassword).to.not.eq(undefined);
+    //     expect(response.body._id).to.not.eq(undefined);
+    //
+    //     expect(response.body.nome).to.eq('QA Teste');
+    //     expect(userEmail).to.eq('teste_1@qa.com');
+    //     expect(userPassword).to.eq('123');
+    //     expect(response.body.administrador).to.eq('true');
+    //
+    //     Cypress.env('userEmail', userEmail);
+    //     Cypress.env('userPassword', userPassword);
+    //
+    //     expect(userEmail).to.eq(Cypress.env('userEmail'));
+    //     expect(userPassword).to.eq(Cypress.env('userPassword'));
+    // })
+    // });
+})
+
+
+Cypress.Commands.add('teardownUser', () => {
+    console.log('tear down user');
+    console.log(`id: ${Cypress.env('userId')}`);
+    cy.request({
+        url: `https://serverest.dev/usuarios/${Cypress.env('userId')}`,
+        method: 'DELETE',
+        body: {},
+        // headers: `${Cypress.env('tokenAuthorization')}`
+    }).then(response => {
+        console.log('delete');
+        expect(response.status).to.eq(200);
+        console.log(response);
+        expect(response.body.message).to.eq('Registro excluído com sucesso');
+    });
+
+    cy.request({
+        url: `https://serverest.dev/usuarios/${Cypress.env('userId')}`,
+        method: 'GET',
+        failOnStatusCode: false  // Evita que o Cypress falhe automaticamente em caso de status de erro
+    }).then(response => {
+        console.log('deletado');
+        expect(response.status).to.eq(400);
+        console.log(response);
+        expect(response.body.message).to.eq('Usuário não encontrado');
+
+        expect(response.body.nome).to.eq(undefined);
+        expect(response.body.email).to.eq(undefined);
+        expect(response.body.password).to.eq(undefined);
+        expect(response.body._id).to.eq(undefined);
+
+    });
+})
