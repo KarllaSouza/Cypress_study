2a66f3ced1e0c509be0d3dcb138811b7083ee2ad
KarllaSouza <karlladanielly94@hotmail.com>
2024-11-29 20:59:28 +0000

    update apiServerest-product.cy.js file

--- cypress/e2e/api_testing/apiServerest-product.cy.js
+++ cypress/e2e/api_testing/apiServerest-product.cy.js
@@ -15,7 +15,7 @@ describe('API Tests - Product', () => {
                 method: 'GET',
                 failOnStatusCode: false
             }).then(response => {
-                console.log('create: '+response.body);
+                console.log('create: ' + response.body);
                 console.log('token - no arquivo de produto: ' + JSON.stringify(response.body));
                 expect(200).to.eq(response.status);
                 expect(undefined).to.not.eq(response.body.quantidade);
@@ -37,9 +37,8 @@ describe('API Tests - Product', () => {
                     headers: {
                         Authorization: token.token,
                     }
-                //    failOnStatusCode: false
                 }).then(response => {
-                    console.log('create: '+ JSON.stringify(response.body));
+                    console.log('create: ' + JSON.stringify(response.body));
                     expect(201).to.eq(response.status);
                     expect('Cadastro realizado com sucesso').to.eq(response.body.message);
                     expect(undefined).to.not.eq(response.body._id);
@@ -49,13 +48,12 @@ describe('API Tests - Product', () => {
             });
         });
 
-        it('Case 9.3: Find product - after create', ()=>{
+        it('Case 9.3: Find product - after create', () => {
             cy.request({
                 url: `https://serverest.dev/produtos/${Cypress.env('productId')}`,
                 method: 'GET'
-          //      failOnStatusCode: false
             }).then(response => {
-                console.log('find: '+ JSON.stringify(response.body));
+                console.log('find: ' + JSON.stringify(response.body));
                 expect(200).to.eq(response.status);
                 expect(undefined).to.not.eq(response.body.nome);
                 expect(undefined).to.not.eq(response.body._id);
@@ -65,25 +63,41 @@ describe('API Tests - Product', () => {
             })
         });
 
-    //    it('Case 9.4: Update product', ()=>{
-            // cy.request({
-            //     url: 'https://serverest.dev/produtos/',
-            //     method: '',
-            //     body: {}
-            // }).then(response => {
-            //     expect(response.status).to.eq()
-            // })
-    //    });
+        it('Case 9.4: Update product', () => {
+            cy.get('@token').then((token) => {
+                cy.request({
+                    url: `https://serverest.dev/produtos/${Cypress.env('productId')}`,
+                    method: 'PUT',
+                    body: {
+                        "nome": "Mouse kds - editado",
+                        "preco": 65,
+                        "descricao": "Produto teste 1 - editado",
+                        "quantidade": 150
+                    },
+                    headers: {
+                        Authorization: token.token,
+                    }
+                }).then(response => {
+                    expect(200).to.eq(response.status);
+                    expect('Registro alterado com sucesso').to.eq(response.body.message);
+                })
+            });
+        });
 
-    //    it('Case 9.5: Find product - after update', ()=>{
-            // cy.request({
-            //     url: 'https://serverest.dev/produtos/',
-            //     method: '',
-            //     body: {}
-            // }).then(response => {
-            //     expect(response.status).to.eq()
-            // })
-    //    });
+        it('Case 9.5: Find product - after update', () => {
+            cy.request({
+                url: `https://serverest.dev/produtos/${Cypress.env('productId')}`,
+                method: 'GET'
+            }).then(response => {
+                console.log('find: ' + JSON.stringify(response.body));
+                expect(200).to.eq(response.status);
+                expect('Mouse kds - editado').to.eq(response.body.nome);
+                expect(`${Cypress.env('productId')}`).to.eq(response.body._id);
+                expect(65).to.eq(response.body.preco);
+                expect('Produto teste 1 - editado').to.eq(response.body.descricao);
+                expect(150).to.eq(response.body.quantidade);
+            })
+        });
 
         it('Case 9.6: Delete product', () => {
             console.log('case: delete')
@@ -95,44 +109,112 @@ describe('API Tests - Product', () => {
                     headers: {
                         Authorization: token.token,
                     }
-                //    failOnStatusCode: false
                 }).then(response => {
-                    console.log('delete: '+ JSON.stringify(response.body));
+                    console.log('delete: ' + JSON.stringify(response.body));
                     expect(200).to.eq(response.status);
                     expect('Registro excluído com sucesso').to.eq(response.body.message);
                 })
             });
-    });
+        });
 
-        it('Case 9.7: Find product - after delete', ()=>{
+        it('Case 9.7: Find product - after delete', () => {
             cy.request({
                 url: `https://serverest.dev/produtos/${Cypress.env('productId')}`,
                 method: 'GET',
                 failOnStatusCode: false
             }).then(response => {
-                console.log('find after delete: '+ JSON.stringify(response.body));
+                console.log('find after delete: ' + JSON.stringify(response.body));
                 expect(400).to.eq(response.status);
                 expect('Produto não encontrado').to.eq(response.body.message);
             })
         });
 
-
-
-        //     it('', () => {
-        //         cy.request({
-        //             url: '',
-        //             method: '',
-        //             body: {}
-        //         }).then(response => {
-        //             expect(response.status).to.eq()
-        //             console.log('user ID: ' + response.body._id)
-        //             Cypress.env('user_id', response._id)
-        //         })
-        //     });
     });
 
-   afterEach(() => {
+    context('Scenario 10 (success): Create product through PUT', () => {
+        it('Case 10.1: Update product', () => {
+            cy.get('@token').then((token) => {
+                cy.request({
+                    url: `https://serverest.dev/produtos/000000000001`,
+                    method: 'PUT',
+                    body: {
+                        "nome": "teclado kds",
+                        "preco": 40,
+                        "descricao": "Produto teste cadastrado no PUT",
+                        "quantidade": 20
+                    },
+                    headers: {
+                        Authorization: token.token,
+                    }
+                }).then(response => {
+                    expect(201).to.eq(response.status);
+                    expect('Cadastro realizado com sucesso').to.eq(response.body.message);
+                    expect(undefined).to.not.eq(response.body._id);
+                    const productId2 = response.body._id;
+                    Cypress.env('productId2', response.body._id)
+                })
+            });
+        });
+
+        it('Case 10.2: Find product - after create throught put', () => {
+            cy.request({
+                url: `https://serverest.dev/produtos/${Cypress.env('productId2')}`,
+                method: 'GET'
+            }).then(response => {
+                console.log('find: ' + JSON.stringify(response.body));
+                expect(200).to.eq(response.status);
+                expect(undefined).to.not.eq(response.body.nome);
+                expect(undefined).to.not.eq(response.body._id);
+                expect(undefined).to.not.eq(response.body.preco);
+                expect(undefined).to.not.eq(response.body.descricao);
+                expect(undefined).to.not.eq(response.body.quantidade);
+            })
+        });
+
+        it('Case 10.3: Delete product - created throught put', () => {
+            cy.get('@token').then((token) => {
+                cy.request({
+                    url: `https://serverest.dev/produtos/${Cypress.env('productId2')}`,
+                    method: 'DELETE',
+                    headers: {
+                        Authorization: token.token,
+                    }
+                }).then(response => {
+                    expect(200).to.eq(response.status);
+                    expect('Registro excluído com sucesso');
+                })
+            });
+        });
+
+        it('Case 10.4: Find product - after delete', () => {
+            cy.request({
+                url: `https://serverest.dev/produtos/${Cypress.env('productId2')}`,
+                method: 'GET',
+                failOnStatusCode: false
+            }).then(response => {
+                console.log('find after delete: ' + JSON.stringify(response.body));
+                expect(400).to.eq(response.status);
+                expect('Produto não encontrado').to.eq(response.body.message);
+            })
+        });
+
+    })
+
+    afterEach(() => {
         cy.deleteUserFunction();
         console.log('deletado');
     });
 })
+
+
+//     it('', () => {
+//         cy.request({
+//             url: '',
+//             method: '',
+//             body: {}
+//         }).then(response => {
+//             expect(response.status).to.eq()
+//             console.log('user ID: ' + response.body._id)
+//             Cypress.env('user_id', response._id)
+//         })
+//     });
